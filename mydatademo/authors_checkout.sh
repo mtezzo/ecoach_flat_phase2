4ac1215d842c7dccd93d2fbc69ad6fc916dff4b7
Michael Ezzo <ezzomich@msu.edu>
2014-11-10 15:36:24 -0500

    fixed script path

--- mydatademo/authors_checkout.sh
+++ mydatademo/authors_checkout.sh
@@ -3,12 +3,12 @@
 #export SCRIPT_DIR=$(pwd)
 
 echo 'begin script' 
-cd /usr/local/ecoach_webapps/mts/mtsdemo
+cd /var/www/ecoach_webapps/mts/mtsdemo
 svn update
 #svn update -r1444 mts.dictionary
 #svn update -r1574 mts.dictionary
-source /usr/local/ecoach_webapps/env/v1/bin/activate
-python /usr/local/ecoach_webapps/manage.py collectstatic --noinput --settings=mydatademo.settings
+source /var/www/ecoach_webapps/env/v1/bin/activate
+python /var/www/ecoach_webapps/manage.py collectstatic --noinput --settings=mydatademo.settings
 echo 'end of script'
 
 
